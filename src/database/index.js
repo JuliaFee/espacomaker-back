1783009063c3a2738081811d78a91d0a97a10dff
JuliaFee <julia.silva133@aluno.senai.br>
2024-09-18 16:33:00 -0300

    index db changes

--- src/database/index.js
+++ src/database/index.js
@@ -9,33 +9,23 @@ const user = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
 const host = process.env.DB_HOST;
 const port = process.env.DB_PORT;
-const database = process.env.DB_NAME;
+const database = process.env.DB_DATABASE;
 
-const dbURL =
-  "postgres://" +
-  user +
-  ":" +
-  password +
-  "@" +
-  host +
-  ":" +
-  port +
-  "/" +
-  database;
+const dbURL = 'postgres://' + user + ':' + password + '@' + host + ':' + port + '/' + database;
+
 
 const db = pgp()(dbURL);
 
-export function createConnection() {
-  db.query("SELECT 1 + 1 AS result").then((result) => {
-    console.log(result);
-  });
+export function createConnection(){
+    db.query("SELECT 1 + 1 AS result").then ((result) => {
+        console.log(result);
+    });
 }
 
-// Captura o caminho do arquivo atual
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const filePath = join(__dirname, "create-tables.sql");
+const filePath = join(__dirname, "script.sql");
 const query = new pgp.QueryFile(filePath);
 
 db.query(query);
