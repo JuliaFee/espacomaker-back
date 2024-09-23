611156a7687a243ba66f9703111c87e1577cd5ae
JuliaFee <julia.silva133@aluno.senai.br>
2024-09-23 10:36:21 -0300

    reserva repository fixed

--- src/models/reservas/ReservasRepository.js
+++ src/models/reservas/ReservasRepository.js
@@ -1,38 +1,74 @@
-export class ReservasRepository {
-    constructor() {
-      this.reservas = [];
-    }
-  
-    getReserva() {
-      return this.reservas;
-    }
-  
-    getReservaById(id) {
-      return this.reservas.find((reserva) => reserva.id === id);
-    }
-  
-    addReserva(reserva) {
-      this.reservas.push(reserva);
-    }
-  
-    updateReserva(id, id_user, id_ferramenta, id_impressora, data_reserva, hora_inicio, hora_fim, status_reserva) {
-      const reserva = this.getReservaById(id);
+import db from "../../db/index.js";
 
-      if (reserva) {
-        reserva.id_user = id_user;
-        reserva.id_ferramenta = id_ferramenta;
-        reserva.id_impressora = id_impressora;
-        reserva.data_reserva = data_reserva;
-        reserva.hora_inicio = hora_inicio;
-        reserva.hora_fim = hora_fim;
-        reserva.status_reserva = status_reserva;
-      }
-      
-      return reserva;
-    }
-  
-    deleteReserva(id) {
-      this.reservas = this.reservas.filter((reserva) => reserva.id !== id);
+class ReservaList {
+  constructor() {
+    this.db = db;
+  }
+
+  async getReservas() {
+    try {
+      const allReservas = await this.db.manyOrNone("SELECT * FROM reserva");
+      return allReservas;
+    } catch (error) {
+      console.error("Failed to get reservas:", error);
+      throw error; 
     }
   }
-  
+
+  async getReservaById(id) {
+    try {
+      const reserva = await this.db.oneOrNone(
+        "SELECT * FROM reserva WHERE id = $1",
+        id
+      );
+      return reserva;
+    } catch (error) {
+      console.error(`Failed to get reserva by id ${id}:`, error);
+      throw error;
+    }
+  }
+
+  async registerReserva(reserva) {
+    try {
+      await this.db.none(
+        "INSERT INTO reserva (id_user, id_ferramenta, id_impressora, data_reserva, hora_inicio, hora_fim, status_reserva) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+        [reserva.id_user, reserva.id_ferramenta, reserva.id_impressora, reserva.data_reserva, reserva.hora_inicio, reserva.hora_fim, reserva.status_reserva]
+      );
+      return reserva;
+    } catch (error) {
+      console.error("Failed to register reserva:", error);
+      throw error; 
+    }
+  }
+
+  async updateReserva(id, id_user, id_ferramenta, id_impressora, data_reserva, hora_inicio, hora_fim, status_reserva) {
+    try {
+      const reserva = await this.getReservaById(id);
+
+      if (!reserva) {
+        return null;
+      }
+
+      const updatedReserva = await this.db.one(
+        "UPDATE reserva SET id_user = $1, id_ferramenta = $2, id_impressora = $3, data_reserva = $4, hora_inicio = $5, hora_fim = $6, status_reserva = $7 WHERE id = $8 RETURNING *",
+        [id_user, id_ferramenta, id_impressora, data_reserva, hora_inicio, hora_fim, status_reserva, id]
+      );
+
+      return updatedReserva;
+    } catch (error) {
+      console.error(`Failed to update reserva ${id}:`, error);
+      throw error; 
+    }
+  }
+
+  async deleteReserva(id) {
+    try {
+      await this.db.none("DELETE FROM reserva WHERE id = $1", id);
+    } catch (error) {
+      console.error(`Failed to delete reserva ${id}:`, error);
+      throw error; 
+    }
+  }
+}
+
+export default ReservaList;
