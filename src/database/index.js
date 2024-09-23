0cc844a388085d7240817d291595f5b335e791f2
maria eduarda <123502092+Mariavalonga@users.noreply.github.com>
2024-09-23 09:31:41 -0300

    db config - ju e le

--- src/database/index.js
+++ src/database/index.js
@@ -9,7 +9,7 @@ const user = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
 const host = process.env.DB_HOST;
 const port = process.env.DB_PORT;
-const database = process.env.DB_DATABASE;
+const database = process.env.DB_NAME;
 
 const dbURL = 'postgres://' + user + ':' + password + '@' + host + ':' + port + '/' + database;
 
