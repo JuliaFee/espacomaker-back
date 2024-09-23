390c4a857f7d97215b97023ebb234c275d7fbdeb
JuliaFee <julia.silva133@aluno.senai.br>
2024-09-23 11:22:05 -0300

    controller ajustes

---
+++ src/controllers/ferramentas.controller.js
@@ -0,0 +1,74 @@
+import db from "../../db/index.js";
+
+class FerramentaList {
+  constructor() {
+    this.db = db;
+  }
+
+  async getFerramentas() {
+    try {
+      const allFerramentas = await this.db.manyOrNone("SELECT * FROM ferramentas");
+      return allFerramentas;
+    } catch (error) {
+      console.error("Failed to get ferramentas:", error);
+      throw error; 
+    }
+  }
+
+  async getFerramentaById(id) {
+    try {
+      const ferramenta = await this.db.oneOrNone(
+        "SELECT * FROM ferramentas WHERE id = $1",
+        id
+      );
+      return ferramenta;
+    } catch (error) {
+      console.error(`Failed to get ferramenta by id ${id}:`, error);
+      throw error;
+    }
+  }
+
+  async registerFerramenta(ferramenta) {
+    try {
+      await this.db.none(
+        "INSERT INTO ferramentas (nome, descricao, img, status) VALUES ($1, $2, $3, $4)",
+        [ferramenta.nome, ferramenta.descricao, ferramenta.img, ferramenta.status]
+      );
+      return ferramenta;
+    } catch (error) {
+      console.error("Failed to register ferramenta:", error);
+      throw error; 
+    }
+  }
+
+  async updateFerramenta(id, nome, descricao, img, status) {
+    try {
+      const ferramenta = await this.getFerramentaById(id);
+
+      if (!ferramenta) {
+        return null;
+      }
+
+      const updatedFerramenta = await this.db.one(
+        "UPDATE ferramentas SET nome = $1, descricao = $2, img = $3, status = $4 WHERE id = $5 RETURNING *",
+        [nome, descricao, img, status, id]
+      );
+
+      return updatedFerramenta;
+    } catch (error) {
+      console.error(`Failed to update ferramenta ${id}:`, error);
+      throw error; 
+    }
+  }
+
+  async deleteFerramenta(id) {
+    try {
+      await this.db.none("DELETE FROM ferramentas WHERE id = $1", id);
+    } catch (error) {
+      console.error(`Failed to delete ferramenta ${id}:`, error);
+      throw error; 
+    }
+  }
+}
+
+export default FerramentaList;
---
+++ src/controllers/impressora.controller.js
@@ -0,0 +1,92 @@
+import Impressora from "../models/impressora/Impressora.js";
+import ImpressoraList from "../models/impressora/ImpressoraList.js";
+
+const impressoraRepository = new ImpressoraList();
+
+export const getImpressora = async (req, res) => {
+    try {
+        const impressoras = await impressoraRepository.getImpressoras();
+        if (!impressoras || impressoras.length === 0) {
+            return res.status(404).send({ message: "Não há impressoras" });
+        }
+        return res.status(200).send({ totalImpressoras: impressoras.length, impressoras });
+    } catch (error) {
+        return res
+            .status(500)
+            .send({ message: "Erro ao buscar impressoras", error: error.message });
+    }
+}
+
+export const getImpressoraById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const impressora = await impressoraRepository.getImpressoraById(id);
+        if (!impressora) {
+            return res.status(404).send({ message: "Impressora não encontrada" });
+        }
+        return res.status(200).send({ message: "Impressora encontrada", impressora });
+    } catch (error) {
+        return res
+            .status(500)
+            .send({ message: "Erro ao buscar impressora", error: error.message });
+    }
+}
+
+export const addImpressora = async (req, res) => {
+    try {
+        const { nome, descricao, img, status, valor } = req.body;
+        const newImpressora = new Impressora(null, nome, descricao, img, status, valor);
+        const impressora = await impressoraRepository.registerImpressora(newImpressora);
+        return res
+            .status(201)
+            .send({ message: "Impressora criada com sucesso", impressora });
+    
+    } catch (error) {
+        return res
+            .status(500)
+            .send({ message: "Erro ao criar impressora", error: error.message });
+    }
+}
+
+
+export const updateImpressora = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { nome, descricao, img, status, valor } = req.body;
+        const impressoraById = await impressoraRepository.getImpressoraById(id);
+        if (!impressoraById) {
+            return res.status(404).send({ message: "Impressora não encontrada" });
+        }
+
+        const updateImpressora = await impressoraRepository.updateImpressora(
+            id,
+            nome, 
+            descricao, 
+            img, 
+            status, 
+            valor);
+        return res
+            .status(200)
+            .send({ message: "Impressora atualizada com sucesso", updateImpressora });
+    } catch (error) {
+        return res
+            .status(500)
+            .send({ message: "Erro ao atualizar impressora", error: error.message });
+    }
+}
+
+export const deleteImpressora = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const impressora = await impressoraRepository.getImpressoraById(id);
+        if (!impressora) {
+            return res.status(404).send({ message: "Impressora não encontrada" });
+        }
+        await impressoraRepository.deleteImpressora(id);
+        return res.status(200).send({ message: "Impressora deletada com sucesso" });
+    } catch (error) {
+        return res
+            .status(500)
+            .send({ message: "Erro ao deletar impressora", error: error.message });
+    }
+}
