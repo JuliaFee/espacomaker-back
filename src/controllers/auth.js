f757c05e3fa06475e5248926a843ced56a02fae4
Letícia <leticia.bizaglio@aluno.senai.br>
2024-10-07 07:38:06 -0300

    alterações

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -5,11 +5,10 @@ const generateToken = (user) => {
   const payload = {
     id: user.id,
     email: user.email,
-    // Add other relevant information here
   };
 
   const token = jwt.sign(payload, process.env.SECRET_KEY, {
-    expiresIn: '1h', // Token expires in 1 hour
+    expiresIn: '1h', 
   });
 
   return token;
@@ -19,18 +18,15 @@ export const login = async (req, res) => {
   try {
     const { email, senha } = req.body;
 
-    // Verifica se o usuário existe
     const user = await UserList.getUserByEmail(email);
     if (!user) {
       return res.status(404).send({ message: "Usuário não encontrado" });
     }
 
-    // Verifica se a senha está correta
     if (user.senha !== senha) {
       return res.status(401).send({ message: "Senha incorreta" });
     }
 
-    // Se tudo estiver ok, retorna um token de acesso
     const token = generateToken(user);
     return res.status(200).send({ message: "Login realizado com sucesso", token });
   } catch (error) {
