b26737de92ef77be71e5506f7a03c8ea7af02b55
Amanda dos Santos Silva <123501998+Amandamoonchild@users.noreply.github.com>
2024-10-21 11:05:43 -0300

    pls go

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { config } from "dotenv";
 import { router } from "./routes/index.routes.js";
+import cors from "cors"; // Use 'import' instead of 'require'
 
 config();
 
@@ -8,6 +9,7 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
+app.use(cors()); // Certifique-se que isso vem antes do router
 app.use(router);
 
 app.listen(port, () =>
