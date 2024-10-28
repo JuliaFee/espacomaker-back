eeb4c9b160d3106267c5f77f41e5dd9972877268
Letícia <leticia.bizaglio@aluno.senai.br>
2024-10-28 09:03:03 -0300

    login e register user

--- src/models/user/UsersList.js
+++ src/models/user/UsersList.js
@@ -5,37 +5,28 @@ export default class UserList {
     this.db = db;
   }
 
-  // Método para obter todos os usuários
   async getUsers() {
     try {
-      const allUsers = await this.db.manyOrNone("SELECT * FROM users");
-      return allUsers;
+      return await this.db.manyOrNone("SELECT * FROM users");
     } catch (error) {
       console.error("Falha ao obter usuários:", error.message);
       throw error;
     }
   }
 
-  // Método para obter um usuário pelo ID
   async getUserById(id) {
-    console.log(`Fetching user with ID: ${id}`);
-    const user = await this.db.oneOrNone("SELECT * FROM users WHERE id = $1", [id]);
-    return user;
-}
+    return await this.db.oneOrNone("SELECT * FROM users WHERE id = $1", [id]);
+  }
 
-
-  // Método para obter um usuário pelo email
   async getUserByEmail(email) {
     try {
-      const user = await this.db.oneOrNone("SELECT * FROM users WHERE email = $1", [email]);
-      return user;
+      return await this.db.oneOrNone("SELECT * FROM users WHERE email = $1", [email]);
     } catch (error) {
       console.error(`Falha ao obter usuário por email ${email}:`, error.message);
       throw error;
     }
   }
 
-  // Método para adicionar um novo usuário
   async addUser(user) {
     try {
       await this.db.none(
@@ -49,16 +40,8 @@ export default class UserList {
     }
   }
 
-  // Método para atualizar um usuário existente
   async updateUser(id, nome, email, senha, tipo) {
     try {
-      const user = await this.getUserById(id);
-
-      if (!user) {
-        console.error(`Usuário com ID ${id} não encontrado.`);
-        return null;
-      }
-
       const updatedUser = await this.db.one(
         "UPDATE users SET nome = $1, email = $2, senha = $3, tipo = $4 WHERE id = $5 RETURNING *",
         [nome, email, senha, tipo, id]
@@ -71,18 +54,9 @@ export default class UserList {
     }
   }
 
-  // Método para deletar um usuário
   async deleteUser(id) {
     try {
-      const user = await this.getUserById(id);
-
-      if (!user) {
-        console.error(`Usuário com ID ${id} não encontrado.`);
-        return null;
-      }
-
       await this.db.none("DELETE FROM users WHERE id = $1", [id]);
-      return { message: `Usuário com ID ${id} foi deletado com sucesso.` };
     } catch (error) {
       console.error(`Falha ao deletar usuário com ID ${id}:`, error.message);
       throw error;
