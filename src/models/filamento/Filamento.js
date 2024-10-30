a365f36447f15793ee4e7e12dcebea6074d3540f
Letícia <leticia.bizaglio@aluno.senai.br>
2024-10-30 08:19:23 -0300

    ajuste filamento

--- src/models/filamento/Filamento.js
+++ src/models/filamento/Filamento.js
@@ -1,12 +1,10 @@
-
 export default class Filamento {
     constructor(id_impressora, tipo, cor, quantidade, valor_por_kg) {
-        this.id_impressora = id_impressora;
-        this.tipo = tipo;
-        this.cor = cor;
-        this.quantidade = quantidade;
-        this.valor_por_kg = valor_por_kg;
+      this.id_impressora = id_impressora;
+      this.tipo = tipo;
+      this.cor = cor;
+      this.quantidade = quantidade;
+      this.valor_por_kg = valor_por_kg;
     }
-
-    
-}
+  }
+  
