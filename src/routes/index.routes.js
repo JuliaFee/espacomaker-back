17c281121723e0e8f4ff01b99756732ddc742ce1
Letícia <leticia.bizaglio@aluno.senai.br>
2024-11-13 10:31:07 -0300

    index

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import ferramentaRouter from "./ferramentas.routes.js";
 import impressoraRouter from "./impressora.routes.js";
-import reservasRouter from "./reservas.routes.js";
+import reservasRouterImpressora from "./reserva_impressora.routes.js";
+import reservasRouterFerramenta from "./reserva_ferramenta.routes.js";
 import usersRouter from "./users.routes.js";
 import horarioRouter from "./horario.routes.js";
 import filamentoRouter from "./filamento.routes.js";
@@ -17,7 +18,8 @@ router.get("/", (req, res) => {
 router.use("/ferramentas", ferramentaRouter);
 router.use("/horarios", horarioRouter);
 router.use("/impressora", impressoraRouter);
-router.use("/reservas", reservasRouter);
+router.use("/reservas-impressora", reservasRouterImpressora);
+router.use("/reservas-ferramenta", reservasRouterFerramenta);
 router.use("/users", usersRouter);
 router.use("/filamentos", filamentoRouter);
 router.use("/login", authRouter); 
